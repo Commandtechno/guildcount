4f53b565a4fd20e0525199e736ee53dbdc545b39
ItsRauf <raufislam2003@gmail.com>
2021-04-23 19:32:33 -0400

    fix(paramCheck): make sure to use https

--- src/paramCheck.js
+++ src/paramCheck.js
@@ -1,7 +1,7 @@
 if (window.location.hash.length === 0) {
   window.location.replace(
     `https://discord.com/oauth2/authorize?client_id=447809656652562433&redirect_uri=${encodeURIComponent(
-      "http://itsrauf.github.io/guildcount/"
+      "https://itsrauf.github.io/guildcount/"
     )}&response_type=token&scope=guilds%20identify&prompt=none`
   );
 }
